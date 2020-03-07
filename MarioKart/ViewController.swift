43aaff88cd95c325f8e898b9e7ca80c0627d8330
Nanar Boursalian <nanar.boursalian01@gmail.com>
2020-03-06 16:19:53 -0800

    Initial Commit

---
+++ MarioKart/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MarioKart
+//
+//  Created by Nanar Boursalian on 3/6/20.
+//  Copyright © 2020 NanarBoursalian. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
